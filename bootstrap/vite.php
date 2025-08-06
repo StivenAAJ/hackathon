def048c6c4371a33480da9cf5fb6401ba4d682c0
Stiven Arguello Jimenez <stivenjim2005@gmail.com>
2025-08-05 19:53:52 -0600

    Create vite.php

---
+++ bootstrap/vite.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'manifest_path' => 'build/manifest.json',
+
+    'dev_server' => [
+        'url' => env('VITE_DEV_SERVER_URL', 'http://localhost:5173'),
+    ],
+
+    'build_directory' => 'build',
+
+];
